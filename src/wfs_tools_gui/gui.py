a7e1dd00042a475b29e4d59554117a742e3352e5
CasuallyCalm <void@some.where>
2024-03-15 16:21:59 -0700

    menu bar added

--- src/wfs_tools_gui/gui.py
+++ src/wfs_tools_gui/gui.py
@@ -10,11 +10,22 @@ dpg.setup_dearpygui()
 
 with dpg.window(label="main_windows", tag='window'):
     with dpg.menu_bar():
-        dpg.add_menu_item(label='help')
+        with dpg.menu(label="Tools"):
+            dpg.add_menu_item(label="Set Tools Path..", tag='tools_dir')
+            with dpg.tooltip("tools_dir"):
+                dpg.add_text("The directory containing the wfs tools executables")
+            dpg.add_menu_item(label="wfs-extract")
+            dpg.add_menu_item(label="wfs-file-injector")
+            dpg.add_menu_item(label="wfs-fuse")
+            dpg.add_menu_item(label="wfs-info")
+            dpg.add_menu_item(label="wfs-reencryptor")
+        
     dpg.add_text("Hello world")
     dpg.add_button(label="Save", callback=save_callback)
     dpg.add_input_text(label="string")
     dpg.add_slider_float(label="float")
+
+
 
 dpg.show_viewport()
 dpg.set_primary_window('window', True)
