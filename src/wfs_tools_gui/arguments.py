78e3133c6c251e1b2c30ea3fdb14fd3389c1a831
CasuallyCalm <void@some.where>
2024-03-23 15:26:22 -0700

    wfs-info working on windows

--- src/wfs_tools_gui/arguments.py
+++ src/wfs_tools_gui/arguments.py
@@ -5,7 +5,7 @@ import dearpygui.dearpygui as dpg
 from .platforms import PLATFORM
 
 
-class Main:
+class Arguments:
     def __init__(self) -> None:
         self.__drives = PLATFORM.get_drives()
 
@@ -15,7 +15,9 @@ class Main:
             dpg.set_value("tools_path", path)
             for item in Path(path).iterdir():
                 if item.is_file() and item.name.lower() in PLATFORM.executables:
-                    dpg.show_item(item.stem.lower() + "_tab")
+                    name = item.stem
+                    dpg.show_item(name + "_tab")
+                    dpg.set_value(name + "_path", item)
             dpg.configure_item(select_otp, default_path=path)
             dpg.configure_item(select_seeprom, default_path=path)
 
@@ -102,27 +104,6 @@ class Main:
 
         with dpg.group():
             with dpg.group(horizontal=True):
-                dpg.add_button(
-                    label="Select wfs-tools directory",
-                    callback=lambda: dpg.show_item(select_tools),
-                )
-                dpg.add_input_text(tag="tools_path", readonly=True)
-
-            with dpg.group(horizontal=True):
-                dpg.add_button(
-                    label="Select otp file (otp.bin)",
-                    callback=lambda: dpg.show_item(select_otp),
-                )
-                dpg.add_input_text(tag="otp_path", readonly=True)
-
-            with dpg.group(horizontal=True):
-                dpg.add_button(
-                    label="Select seeprom file (seeprom.bin)",
-                    callback=lambda: dpg.show_item(select_seeprom),
-                )
-                dpg.add_input_text(tag="seeprom_path", readonly=True)
-
-            with dpg.group(horizontal=True):
                 dpg.add_text("Input Type: ")
                 dpg.add_radio_button(
                     tag="input_type",
@@ -133,40 +114,63 @@ class Main:
                 )
 
             with dpg.group(horizontal=True):
-                dpg.add_text("Input:")
-
                 with dpg.group(horizontal=True, tag="usb_group"):
+                    dpg.add_button(
+                        label="Refresh Drives", callback=self.__refresh_drives
+                    )
                     dpg.add_combo(
-                        tag="usb_input",
+                        tag="usb_combo",
                         items=[drive.label for drive in self.__drives],
                         callback=self.__set_input,
                     )
-                    dpg.add_button(label="refresh", callback=self.__refresh_drives)
+                    with dpg.value_registry():
+                        dpg.add_string_value(default_value="", tag='usb_input')
 
                 with dpg.group(horizontal=True, tag="mlc_group", show=False):
                     dpg.add_button(
                         label="Select MLC image:",
                         callback=lambda: dpg.show_item(select_mlc),
                     )
-                    dpg.add_input_text(tag="mlc_input", readonly=True)
+                    dpg.add_input_text(tag="mlc_input")
 
                 with dpg.group(horizontal=True, tag="plain_group", show=False):
                     dpg.add_button(
                         label="Select mount path:",
                         callback=lambda: dpg.show_item(select_plain),
                     )
-                    dpg.add_input_text(tag="plain_input", readonly=True)
+                    dpg.add_input_text(tag="plain_input")
+
+            with dpg.group(horizontal=True):
+                dpg.add_button(
+                    label="Select wfs-tools directory",
+                    callback=lambda: dpg.show_item(select_tools),
+                )
+                dpg.add_input_text(tag="tools_path")
+
+            with dpg.group(horizontal=True, tag="otp_group", show=True):
+                dpg.add_button(
+                    label="Select otp file (otp.bin)",
+                    callback=lambda: dpg.show_item(select_otp),
+                )
+                dpg.add_input_text(tag="otp_path")
+
+            with dpg.group(horizontal=True, tag="seeprom_group", show=True):
+                dpg.add_button(
+                    label="Select seeprom file (seeprom.bin)",
+                    callback=lambda: dpg.show_item(select_seeprom),
+                )
+                dpg.add_input_text(tag="seeprom_path")
 
         dpg.add_spacer(height=10)
 
     def __refresh_drives(self):
-        dpg.set_value("usb_input", "")
+        dpg.set_value("usb_combo", "")
         self.__drives = PLATFORM.get_drives()
-        dpg.configure_item("usb_input", items=[drive.label for drive in self.__drives])
+        dpg.configure_item("usb_combo", items=[drive.label for drive in self.__drives])
 
     def __set_input(self, sender, app_data, user_data):
         for drive in self.__drives:
             if drive == app_data:
+                dpg.set_value('usb_input', drive.path)
                 self.input = drive.path
-
-        print(self.input)
+        
--- src/wfs_tools_gui/gui.py
+++ src/wfs_tools_gui/gui.py
@@ -1,19 +1,38 @@
+import ctypes
+import sys
+
 import dearpygui.dearpygui as dpg
 
+from .arguments import Arguments
 from .platforms import PLATFORM
-from .settings_area import Main
 from .style import register_font
 from .tool_tabs import WFSExtract, WFSFileInjector, WFSFuse, WFSInfo, WFSReencryptor
 
 
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
+
 def run():
+    if PLATFORM.name == 'win':
+        if is_admin():
+            gui()
+        else:
+            ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+
+    elif PLATFORM.name == 'linux':
+        gui()
+
+def gui():
     dpg.create_context()
     dpg.create_viewport(title="WFS Tools", width=800, height=600)
     dpg.setup_dearpygui()
     register_font()
 
-    with dpg.window(label="main_windows", tag="window"):
-        Main()
+    with dpg.window( tag="window"):
+        Arguments()
         with dpg.tab_bar(tag="tool_tab_bar"):
             WFSExtract()
             WFSFileInjector()
@@ -25,6 +44,7 @@ def run():
     dpg.set_primary_window("window", True)
     dpg.start_dearpygui()
     dpg.destroy_context()
+
 
 
 if __name__ == "__main__":
