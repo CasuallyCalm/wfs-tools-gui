d6ec5d91d608a2abc7af72d7c9be6cb6c5626191
CasuallyCalm <void@some.where>
2024-03-26 13:21:52 -0700

    sync input groups and radio selection across tabs

--- src/wfs_tools_gui/arguments/input_type.py
+++ src/wfs_tools_gui/arguments/input_type.py
@@ -2,41 +2,37 @@ import dearpygui.dearpygui as dpg
 
 from ..platforms import PLATFORM
 
+INPUT_GROUPS = dict(usb=[], mlc=[], plain=[])
+RADIOS = []
+
 
 def input_type(parent: int | str):
     drives = PLATFORM.get_drives()
-    _id = dpg.add_text("input", show=False, parent=parent)
-
-    def uid(end: str):
-        return str(_id) + end
-
-    usb_combo = uid("usb_combo")
-    usb_group = uid("usb_group")
-    mlc_group = uid("mlc_group")
-    plain_group = uid("plain_group")
 
     def refresh_drives():
         dpg.set_value(usb_combo, "")
         __drives = PLATFORM.get_drives()
         dpg.configure_item(usb_combo, items=[drive.label for drive in __drives])
 
-    def set_input(sender, app_data, user_data):
+    def set_input(_, app_data, __):
         for drive in drives:
             if drive == app_data:
                 dpg.set_value("--input usb", drive.path)
 
     def change_input_type(_, input_type: str):
-        for item in ["usb", "mlc", "plain"]:
-            group = uid(item + "_group")
-            if item != input_type.lower():
-                dpg.hide_item(group)
-            else:
-                dpg.show_item(group)
+        for radio in RADIOS:
+            dpg.set_value(radio, input_type)
+        for _type, item_group in INPUT_GROUPS.items():
+            for item in item_group:
+                if _type != input_type.lower():
+                    dpg.hide_item(item)
+                else:
+                    dpg.show_item(item)
 
     with dpg.group(parent=parent):
         with dpg.group(horizontal=True):
             dpg.add_text("Input Type: ")
-            dpg.add_radio_button(
+            radio = dpg.add_radio_button(
                 items=["USB", "MLC", "Plain"],
                 horizontal=True,
                 callback=change_input_type,
@@ -44,10 +40,10 @@ def input_type(parent: int | str):
             )
 
         with dpg.group(horizontal=True):
-            with dpg.group(horizontal=True, tag=usb_group):
+            with dpg.group(horizontal=True) as usb:
                 dpg.add_button(label="Refresh Drives", callback=refresh_drives)
-                dpg.add_combo(
-                    tag=usb_combo,
+                usb_combo = dpg.add_combo(
+                    # tag=usb_combo,
                     items=[drive.label for drive in drives],
                     callback=set_input,
                     source="usb_label",
@@ -55,16 +51,22 @@ def input_type(parent: int | str):
                 with dpg.value_registry():
                     dpg.add_string_value(default_value="", source="--input usb")
 
-            with dpg.group(horizontal=True, tag=mlc_group, show=False):
+            with dpg.group(horizontal=True, show=False) as mlc:
                 dpg.add_button(
                     label="Select MLC image:",
                     callback=lambda: dpg.show_item("mlc_dialog"),
                 )
                 dpg.add_input_text(source="--input mlc")
 
-            with dpg.group(horizontal=True, tag=plain_group, show=False):
+            with dpg.group(horizontal=True, show=False) as plain:
                 dpg.add_button(
                     label="Select mount path:",
                     callback=lambda: dpg.show_item("plain_dialog"),
                 )
                 dpg.add_input_text(source="--input plain")
+
+            # Used to sync input state across tabs
+            INPUT_GROUPS["usb"].append(usb)
+            INPUT_GROUPS["mlc"].append(mlc)
+            INPUT_GROUPS["plain"].append(plain)
+            RADIOS.append(radio)
